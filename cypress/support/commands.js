d7c4b10ecad72a3a3048fe021882775fa9f2addd
Ethan Gabriel <ethanbenjamin.gabriel@gmail.com>
2024-01-31 10:40:01 -0500

    [EG/SS] fix: remove node modules, .env from git

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,5 @@
+import { baseUrl } from "./constants";
+
 Cypress.Commands.add('statusAndTime', (status, responseTime, code, duration) => {
     expect(status).to.eq(code);
     expect(responseTime).to.be.lessThan(duration);
@@ -6,9 +8,9 @@ Cypress.Commands.add('statusAndTime', (status, responseTime, code, duration) => 
 Cypress.Commands.add('apiRequest', (method, endpoint, body, failOnStatusCode=true) => {
     cy.request({
         method,
-        url : `${Cypress.env('baseUrl')}/${endpoint}`,
+        url : `${baseUrl}/${endpoint}`,
         headers : {
-            'Authorization' : `${Cypress.env('accessToken')}`
+            'Authorization' : `${process.env.BEARER_TOKEN}`
         }, 
         body,
         failOnStatusCode
